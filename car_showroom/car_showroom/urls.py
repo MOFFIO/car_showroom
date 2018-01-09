db166882bbfb554488941a77c2954003f70c8d6e
MOFFIO <moffio.tv@gmail.com>
2018-01-09 14:09:46 +0200

    add some fix

--- car_showroom/car_showroom/urls.py
+++ car_showroom/car_showroom/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^cars_project/', include('cars_project.urls')),
+    url(r'^', include('cars_project.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- car_showroom/cars_project/views.py
+++ car_showroom/cars_project/views.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from .models import Car, CarAttributes, Dealership
+from django.shortcuts import get_object_or_404, render
+from django.views import generic
+from django.utils import timezone
+#from django.template import Context, loader
 
 # Create your views here.
+
+def index(request):
+    car_list = Car.objects.all()
+    context = {'car_list': car_list}
+    return render(request, 'cars_project/index.html', context)
+
+
+
+
